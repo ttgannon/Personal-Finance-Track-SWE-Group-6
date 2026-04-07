60f4e1a185e6e672bfa61cfeb0db2a1d95ffb063
ttgannon <ga1179@nyu.edu>
2026-04-06 21:21:24 -0400

    updating ui

--- frontend/src/hooks/useApi.ts
+++ frontend/src/hooks/useApi.ts
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: '',
+  baseURL: "",
   headers: {
-    'Content-Type': 'application/json'
+    "Content-Type": "application/json",
   },
-  withCredentials: true
+  withCredentials: true,
 });
 
 export async function apiFetch<T>(endpoint: string): Promise<T> {
